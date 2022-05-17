2cce268f1d0e508b6316db483c4a067113e5eb60
Bharathraj Gowda <35210955+Ricchitech@users.noreply.github.com>
2022-05-17 10:05:12 +0530

    SHA data update

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ let sh = forge.md.sha256.create();
 sh.update(value);
 let str = sh.digest().toHex() + "-" + uuidv4();
 console.log(str);
-//fa40405df025d4cefb2b2da493b33ab48063a9c841070fd23b3187169c64d4c1
+//fa40405df025d4cefb2b2da493b33ab48063a9c841070fd23b3187169c64d4c1-044dc4d7-41c3-4b99-a8eb-b7683b69cb23
+
 let arr = [];
 arr=str.split('-');
 console.log(arr[0]);
+//fa40405df025d4cefb2b2da493b33ab48063a9c841070fd23b3187169c64d4c1
+
 
 //verifysha
 if (verifysha(arr[0], value)){
-    console.log("Verified");
+    console.log("SHA256 Verified");
 }
 
 function verifysha(sha_data,data){
